5997f07f5cc5dafbf3eed4f55a1f0544886200de
XcoEditz <123192743+XcoEditz@users.noreply.github.com>
2023-03-12 18:24:13 +0000

    Add files via upload

---
+++ ConfigJson.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Ohio_Bot
+{
+    internal struct ConfigJson
+    {
+        [JsonProperty("token")]
+        public string Token { get;private set; }
+        [JsonProperty("prefix")]
+        public string Prefix { get;private set; }
+    }
+}
